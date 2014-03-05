e66d806b7d81c978403df750728e1b04f4839831
Pagendarm <eric.pagendarm@gmail.com>
2014-03-05 13:05:54 -0800

    Was accidentally using Array instead of List in Enviroment, file now compiles

--- src/enviroment.scala
+++ src/enviroment.scala
@@ -5,39 +5,42 @@
 package physics {
     import general._
     import general.Vector3
+    import scala.collection.immutable._
     class Enviroment (
-        protected val time_limit : Long // how long simulation runs
-        protected val delta : Long ) // the resolution of data storage 
+        protected val time_limit : Long , // how long simulation runs
+        protected val delta : Long )    // the resolution of data storage 
         {
         protected var time = 0 : Long // current time of simulation
 
         // Note that persistent forces still are effected by duration
         // but are general to all things in the enviroment
-        protected var persistent_forces = new Array(0) : Array[Force]
+        protected var persistent_forces = List(): List[Force]
 
-        import scala.collection.immutable.Hashmap
-        protected var things = new Hashmap () : Hashmap[String,Record]
+        protected var things = ListMap(): ListMap[String,Record]
 
-        def add_thing (thing: Tuple2[Thing,Array[Force]] ): Unit = 
+        def add_thing (thing: Tuple2[Thing,List[Force]] ): Unit = 
         thing._1 match {
-            case Thing (k,m,i_p,i_v) => things = things + (thing._1.key, 
-                        new Record (m,thing._2,
-                        new Array(Tuple3(0,i_p,i_v)))
-                        )
+            case Thing (k,m,i_p,i_v) => {
+                var arr = List( Tuple3(0 : Long, i_p, i_v) )
+                val rec = new Record (m , thing._2, arr)
+                things = things + Tuple2(thing._1.key, rec)
+                                        }
         }
 
-        def add_force (force: Force)
+        def add_force (force: Force) : Unit = {
+            persistent_forces = force :: persistent_forces
+        }
 
         // Computes the change in velocity (acceleration)
-        def deltaV (f : Vector3, m: Double) : Vector3 = (f * (1.0 / m))
+        def delta_v (f : Vector3, m: Double) : Vector3 = (f * (1.0 / m))
 
-        // Computes the net force by summing over an array of forces
-        def netF (  total: Vector3, 
-                    forces : Array[Force]): Vector3 = 
+        // Computes the net force by summing over an list of forces
+        def net_f (  total: Vector3, 
+                    forces : List[Force]): Vector3 = 
             forces match {
-            case head::tail =>  if (head.duration < time) {
-                                    netF (total + head.value, tail) }
-                                else netF (total, tail)
+            case head :: tail =>  if (head.duration < time) {
+                                    net_f (total + head.value, tail) }
+                                else net_f (total, tail)
             case Nil        => total
         }
 
@@ -45,32 +48,32 @@ package physics {
  
         // Inner class to record each thing's changes
         protected class Record ( val mass : Double, 
-                        var forces : Array[Force] , 
-                        var log : Array[(Long, Vector3, Vector3)] ) {
+                        var forces : List[Force] , 
+                        var log : List[(Long, Vector3, Vector3)] ) {
 
             def update (): Unit = {
             // Either we need to fully update the record
             // with the new values
                 if ( time % delta == 0 ) {
-                    log =   (   time, 
-                            log(0)._2 + log(0)._3 , 
-                            log(0)._3 * delta_v( 
-                                    netF( 
-                                    netF ( Vector3(0,0,0), forces), 
-                                    persistent_forces ) ) 
-                            ) +: log 
+                    log = (   time, 
+                            log.head._2 + log.head._3 , 
+                            log.head._3 + delta_v( 
+                                    net_f ( 
+                                    net_f ( Vector3(0,0,0), forces), 
+                                    persistent_forces) , mass) 
+                            ) :: log 
                 
                }
             // Or we need to just read from the 'top' and update it
             // in place
                 else {
-                    log (0) = (   time, 
-                            log(0)._2 + log[0]._3 , 
-                            log(0)._3 * delta_v( 
-                                    netF( 
-                                    netF ( Vector3(0,0,0), forces), 
-                                    persistent_forces ) ) 
-                            )
+                    log = (   time, 
+                            log.head._2 + log.head._3 , 
+                            log.head._3 + delta_v( 
+                                    net_f ( 
+                                    net_f ( Vector3(0,0,0), forces), 
+                                    persistent_forces) , mass) 
+                            ) :: (log.tail)
                 }
             }
         }
